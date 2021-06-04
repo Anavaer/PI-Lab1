1753f57a5ecb1504633ef2c884ed1c946befa5cd
Anavaer <repcak95@gmail.com>
2021-06-04 19:56:10 +0200

    Add project files.

---
+++ MVC Lab/Controllers/RegistryController.cs
@@ -0,0 +1,59 @@
+﻿using MVC_Lab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Lab.Controllers
+{
+    public class RegistryController : Controller
+    {
+        private readonly ApplicationDbContext dataBase = ApplicationDbContext.Create();
+
+        // GET: Registry
+        public ActionResult Registry(string gender = "M")
+        {
+            var missingPeople = dataBase.MissingPeople.AsQueryable().Where(
+                person => person.Gender == gender
+            );
+        
+            return View(missingPeople.ToList());
+        }
+
+        [Authorize(Roles = "Admin, User")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin, User")]
+        public ActionResult CreateEntry(NewEntryModels data)
+        {
+            try
+            {
+                var missingPersonEntry = new NewEntryModels()
+                {
+                    Name = data.Name,
+                    Surname = data.Surname,
+                    Gender = data.Gender,
+                    Age = data.Age,
+                    City = data.City,
+                    PhotoUrl = data.PhotoUrl
+                };
+
+                dataBase.MissingPeople.Add(missingPersonEntry);
+                dataBase.SaveChanges();
+
+                return RedirectToAction("Registry");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+    }
+
+
+}
---
+++ MVC Lab/Models/NewEntryModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Lab.Models
+{
+    public class NewEntryModels
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Age { get; set; }
+
+        public string City { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+    }
+}
